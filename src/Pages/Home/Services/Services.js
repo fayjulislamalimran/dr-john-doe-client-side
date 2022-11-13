8e2cec0f256be6cc4525ec7409ddebedca3e82e1
Fayjul Islam Al Imran <fayjulislamalimran7@gmail.com>
2022-11-13 12:16:32 +0600

    Services setup all done

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -17,7 +17,7 @@ const Services = () => {
       <div className="text-center mb-10">
         <h3 className="text-2xl text-orange-600 font-bold">Services</h3>
         <p className="text-4xl my-4 font-serif font-semibold uppercase">We give this services my patient</p>
-        {/* <p className="w-3/4 mx-auto">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p> */}
+        <p className="w-3/4 mx-auto">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-14">
         {services.map((service) => (
